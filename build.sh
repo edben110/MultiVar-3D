8797706cf9f489c433c1bc07fea592146646cf51
EdwarBenito <edwar.benitobas@campusucc.edu.co>
2025-10-19 20:03:44 -0500

    Fix: Forzar instalación de webpack-cli y mover babel a dependencies

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ pip install -r requirements.txt
 # Instalar dependencias de Node.js
 npm install
 
-# Construir frontend con webpack
-npx webpack --mode production
+# Asegurar que webpack-cli esté instalado
+npm install webpack webpack-cli --save
+
+# Construir frontend con webpack (usando YES para auto-confirmar)
+echo "yes" | npx webpack --mode production || npx webpack --mode production
 
 # Recopilar archivos estáticos de Django
 cd back
